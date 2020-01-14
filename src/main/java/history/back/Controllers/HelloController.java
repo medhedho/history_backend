c57bf23318435837affe87f3d422ab445eb1e3cc
Mohamed Hedi Houas <mohamedhedihouas@MacBook-Pro-de-Mohamed.local>
2020-01-14 22:34:14 +0100

    stateless ctrl

---
+++ src/main/java/history/back/Controllers/HelloController.java
@@ -0,0 +1,15 @@
+package history.back.Controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
